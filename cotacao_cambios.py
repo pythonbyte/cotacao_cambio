3c8d4be52128e35a009d66db2841e116b2ebc37f
Eduardo Melgaço <dudumelgaco@hotmail.com>
2017-10-09 23:27:17 -0300

    Cotator 0.4
    Adicionado velocidade na leitura do código, interface gráfica e comparação dos preços.

--- cotacao_cambios.py
+++ cotacao_cambios.py
@@ -1,12 +1,27 @@
 #-*- coding: UTF-8 -*-
 
 #Criado por Eduardo Melgaço
-
+#https://github.com/pythonbyte/
 
 import urllib.request
-import json
-from time import strftime
+from tkinter import Tk, Label, Button
+from functools import lru_cache
 
+green_media_dolar = 0
+green_media_dolar_au = 0
+green_media_dolar_ca = 0
+green_media_dolar_nz = 0
+green_media_euro = 0
+green_media_libra = 0
+
+
+
+
+
+
+
+
+@lru_cache(maxsize=None)
 def pega_cotacao():
 	pagina = urllib.request.urlopen('https://www.cotacao.com.br/cotacao-das-moedas.html')
 	texto = pagina.read().decode('utf-8')
@@ -27,9 +42,9 @@ def pega_cotacao():
 	moedas_cot = [dolar_am_cot, dolar_au_cot, dolar_ca_cot, euro_cot, libra_cot, dolar_nz_cot]
 	return moedas_cot
 
-
+@lru_cache(maxsize=None)
 def melhorcambio_dolar_am(x):
-
+	global green_media_dolar
 	url = 'https://www.melhorcambio.com/cotacao/compra/dolar-turismo/belo-horizonte'
 	pagina = urllib.request.urlopen(url)
 	texto = pagina.read().decode('utf-8')
@@ -43,13 +58,13 @@ def melhorcambio_dolar_am(x):
 	final2 = picchioni_local + 593
 	inicio2 = final2 - 4
 	picchioni_cot = float(texto[inicio2:final2].replace(',', '.'))
-	media = (picchioni_cot + confidence_cot + x)/3
+	green_media_dolar  = (picchioni_cot + confidence_cot + x)/3
+	return ('R${:.2f}     -     R${:.2f}     -     R${:.2f}     -     R${:.2f}'.format(confidence_cot, picchioni_cot, x, green_media_dolar))
 
-	print('Confidence Dolar    R${}  - Picchioni Dolar    R${} - Cotacao.com Dolar    R${} - Green Dolar    R${:.2f}'.format(confidence_cot, picchioni_cot, x, media))
-	return media
-
+	
+@lru_cache(maxsize=None)
 def melhorcambio_dolar_au(x):
-
+	global green_media_dolar_au
 	url = 'https://www.melhorcambio.com/cotacao/compra/dolar-australiano/belo-horizonte'
 	pagina = urllib.request.urlopen(url)
 	texto = pagina.read().decode('utf-8')
@@ -63,12 +78,12 @@ def melhorcambio_dolar_au(x):
 	final2 = picchioni_local + 593
 	inicio2 = final2 - 4
 	picchioni_cot_au = float(texto[inicio2:final2].replace(',', '.'))
-	media = (picchioni_cot_au + confidence_cot_au + x)/3
-	print('Confidence Dolar AU R${}  - Picchioni Dolar AU R${} - Cotacao.com Dolar AU R${} - Green Dolar AU R${:.2f}'.format(confidence_cot_au, picchioni_cot_au, x, media))
-	return media
-
+	green_media_dolar_au = (picchioni_cot_au + confidence_cot_au + x)/3
+	return 'R${:.2f}     -     R${:.2f}     -     R${:.2f}     -     R${:.2f}'.format(confidence_cot_au, picchioni_cot_au, x, green_media_dolar_au)
+	
+@lru_cache(maxsize=None)
 def melhorcambio_dolar_ca(x):
-
+	global green_media_dolar_ca
 	url = 'https://www.melhorcambio.com/cotacao/compra/dolar-canadense/belo-horizonte'
 	pagina = urllib.request.urlopen(url)
 	texto = pagina.read().decode('utf-8')
@@ -82,13 +97,13 @@ def melhorcambio_dolar_ca(x):
 	final2 = picchioni_local + 593
 	inicio2 = final2 - 4
 	picchioni_cot_ca = float(texto[inicio2:final2].replace(',', '.'))
-	media = (picchioni_cot_ca + confidence_cot_ca + x)/3
-	print('Confidence Dolar CA R${}  - Picchioni Dolar CA R${} - Cotacao.com Dolar CA R${} - Green Dolar CA R${:.2f}'.format(confidence_cot_ca, picchioni_cot_ca, x, media))
-	return media
+	green_media_dolar_ca = (picchioni_cot_ca + confidence_cot_ca + x)/3
+	return 'R${:.2f}     -     R${:.2f}     -     R${:.2f}     -     R${:.2f}'.format(confidence_cot_ca, picchioni_cot_ca, x, green_media_dolar_ca)
+	
 
-
+@lru_cache(maxsize=None)
 def melhorcambio_euro(x):
-
+	global green_media_euro
 	url = 'https://www.melhorcambio.com/cotacao/compra/euro/belo-horizonte'
 	pagina = urllib.request.urlopen(url)
 	texto = pagina.read().decode('utf-8')
@@ -102,12 +117,12 @@ def melhorcambio_euro(x):
 	final2 = picchioni_local + 593
 	inicio2 = final2 - 4
 	picchioni_cot_euro = float(texto[inicio2:final2].replace(',', '.'))
-	media = (picchioni_cot_euro + confidence_cot_euro + x)/3
-	print('Confidence Euro     R${}  - Picchioni Euro     R${} - Cotacao.com Euro     R${} - Green Euro     R${:.2f}'.format(confidence_cot_euro, picchioni_cot_euro, x, media))
-	return media
-
+	green_media_euro = (picchioni_cot_euro + confidence_cot_euro + x)/3
+	return 'R${:.2f}     -     R${:.2f}     -     R${:.2f}     -     R${:.2f}'.format(confidence_cot_euro, picchioni_cot_euro, x, green_media_euro)
+	
+@lru_cache(maxsize=None)
 def melhorcambio_libra(x):
-
+	global green_media_libra
 	url = 'https://www.melhorcambio.com/cotacao/compra/libra/belo-horizonte'
 	pagina = urllib.request.urlopen(url)
 	texto = pagina.read().decode('utf-8')
@@ -121,12 +136,12 @@ def melhorcambio_libra(x):
 	final2 = picchioni_local + 593
 	inicio2 = final2 - 4
 	picchioni_cot_libra = float(texto[inicio2:final2].replace(',', '.'))
-	media = (picchioni_cot_libra + confidence_cot_libra + x)/3
-	print('Confidence Libra    R${}  - Picchioni Libra    R${} - Cotacao.com Libra    R${} - Green Libra    R${:.2f}'.format(confidence_cot_libra, picchioni_cot_libra, x, media))
-	return media
+	green_media_libra = (picchioni_cot_libra + confidence_cot_libra + x)/3
+	return 'R${:.2f}     -     R${:.2f}     -     R${:.2f}     -     R${:.2f}'.format(confidence_cot_libra, picchioni_cot_libra, x, green_media_libra)
+	
 
 
-
+@lru_cache(maxsize=None)
 def stb():
 	url = 'https://www.stb.com.br/servicos/conversor-de-moedas'
 	pagina = urllib.request.urlopen(url)
@@ -150,10 +165,10 @@ def stb():
 	stb_cot_dolar_ca = float(texto[inicio_ca:final_ca].replace(',', '.'))
 	#print(stb_cot_dolar_ca)
 
-	stb_local_nz = texto.find('ndia</s')
+	stb_local_nz = texto.find('Zelandia</s')
 	final_nz = stb_local_nz + 150
 	inicio_nz = final_nz - 5
-	stb_cot_dolar_nz = float(texto[inicio_nz:final_nz].replace(',', '.'))
+	stb_cot_dolar_nz = texto[inicio_nz:final_nz]
 	#print(stb_cot_dolar_nz)
 
 	stb_local_euro = texto.find('Euro</s')
@@ -170,27 +185,49 @@ def stb():
 	stb_lista = [stb_cot_dolar, stb_cot_dolar_au, stb_cot_dolar_ca, stb_cot_euro, stb_cot_libra, stb_cot_dolar_nz]
 	return stb_lista
 
+print('Programa abrindo... Aguarde...')
+
+g = '''
+{}   -   R${:.2f}\n
+{}   -   R${:.2f}\n
+{}   -   R${:.2f}\n
+{}   -   R${:.2f}\n
+{}   -   R${:.2f}\n'''.format(melhorcambio_dolar_am(pega_cotacao()[0]), stb()[0], melhorcambio_dolar_au(pega_cotacao()[1]), stb()[1], melhorcambio_dolar_ca(pega_cotacao()[2]), stb()[2], melhorcambio_euro(pega_cotacao()[3]), stb()[3], melhorcambio_libra(pega_cotacao()[4]), stb()[4])
+
+f = '''
+   -   R${:.2f}\n
+   -   R${:.2f}\n
+   -   R${:.2f}\n
+   -   R${:.2f}\n
+   -   R${:.2f}\n'''.format((green_media_dolar-stb()[0]),(green_media_dolar_au-stb()[1]),(green_media_dolar_ca-stb()[2]),(green_media_euro -stb()[3]),(green_media_libra-stb()[4]))
+
+
+@lru_cache(maxsize=None)
+class MyFirstGUI:
+    def __init__(self, master):
+    	self.master = master
+    	master.title(' C O T A T O R - 0.3')
+    	master.geometry('490x200')
+    	
+    	self.label = Label(master, text = 'Confidence    Picchioni     Cotação.com    Green       STB      Green-STB')
+    	self.label.pack()
+    	self.label = Label(master, text = 'Dólar AM \n\nDólar AU \n\nDólar CA \n\nEuro \n\nLibra')
+    	self.label.pack(side = 'left')
+    	self.label = Label(master, text=g)
+    	self.label.pack(side = 'left')
+
+    	self.label = Label(master, text=f)
+    	self.label.pack(side = 'left')
+
+
+    	self.close_button = Button(master, text="Close", command= self.master.quit)
+    	self.close_button.pack(side = 'right')
 
 
 
-print('Cotação feita no dia ',strftime("%d %b %Y "))
-melhorcambio_dolar_am(pega_cotacao()[0])
-melhorcambio_dolar_au(pega_cotacao()[1])
-melhorcambio_dolar_ca(pega_cotacao()[2])
-melhorcambio_euro(pega_cotacao()[3])
-melhorcambio_libra(pega_cotacao()[4])
-print('')
-print('Cotação do STB')
-print('')
-print('''
-Cotação do Dolar    R$ {}
-Cotação do Dolar AU R$ {}
-Cotação do Dolar CA R$ {}
-Cotação da Dolar NZ R$ {}
-Cotação do Euro     R$ {}
-Cotação da Libra    R$ {}'''.format(stb()[0], stb()[1], stb()[2],stb()[5], stb()[3], stb()[4]))
-print('')
-print('Dolar NeoZelandes segundo Cotacao.com.br R$ {}'.format(pega_cotacao()[5]))
-fechar = input('Aperte enter para fechar')
+root = Tk()
+my_gui = MyFirstGUI(root)
+print('Programa Aberto')
 
-
+root.mainloop()
+print('Programa Fechado !')
